944c0a358a780b565209978e86064108482d6f5c
majdeddine <majda-jambouri@hotmail.fr>
2017-11-01 14:27:33 +0000

    add test for one journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,4 +3,10 @@ require 'oystercard'
 class Journey
 
 
+
+  def initialize(start, exit)
+    @start = start
+    @exit= exit
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,12 +1,15 @@
 require 'journey'
 
 describe Journey do
-  subject(:journey) { described_class.new }
 
   describe '#initialize' do
     context 'when initialized' do
-      it 'should return an empty journey' do
-        expect(journey.history).to eq []
+      journey = Journey.new('aldgate', 'finchley')
+      it '.current_journey return entry_station' do
+          expect(journey.current_journey[:entry_station]).to eq('aldgate')
+      end
+      it '.current_journey return exit_station' do
+        expect(journey.current_journey[:exit_station]).to eq('finchley')
       end
     end
   end
